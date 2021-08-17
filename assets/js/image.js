57fee9d980b04647cdfd58d212527b5d9f3ebda0
daomapsieucap <bichdao.chau@gmail.com>
2021-08-17 20:39:37 +0700

    v1.0.2: Improve preview image upload

--- assets/js/image.js
+++ assets/js/image.js
@@ -1,5 +1,7 @@
 jQuery(document).ready(function($){
-    //Disable right click on image
+    /**
+     * Disable right click on image
+     */
     $("img").on("contextmenu", function(){
         return false;
     });
