a4bd5dac92a0099347a36c9473db565495fb41c4
daomapsieucap <bichdao.chau@gmail.com>
2021-09-21 20:55:41 +0700

    v1.1.2 - Disable right click on image v1.1.2

--- assets/js/fiber-image.js
+++ assets/js/fiber-image.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($){
     /**
-     * Disable right click on image
+     * Disable right click on image v1.1.2
      */
     $('body').on('contextmenu', 'img', function(e){
         return false;
