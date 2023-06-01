38d21c93fa8d35dec6ad3397921a95dc1314a598
phongkhuu115 <phongkhuu115@gmail.com>
2023-06-01 10:43:52 +0700

    feat(clean-file-name): #13 sanitize %20 in filename

--- includes/filename.php
+++ includes/filename.php
@@ -12,33 +12,23 @@ class Fiber_Admin_Filename{
 	public function __construct(){
 		// Auto Convert File Name
 		if(fiad_get_miscellaneous_option('auto_convert_file_name')){
-			add_filter('add_attachment', [$this, 'fiad_auto_convert_file_name']);
+			add_filter('sanitize_file_name', [$this, 'fiad_auto_convert_file_name'],10,2);
 		}
 	}
 	
 	// Auto Convert File Name
-	public function fiad_auto_convert_file_name($post_id){
-		// Get path info of orginal file
-		$original_path = get_attached_file($post_id);
-		$path_info     = pathinfo($original_path);
-		$dirname       = ev_array_key_exists('dirname', $path_info);
-		$filename      = ev_array_key_exists('filename', $path_info);
-		$extension     = ev_array_key_exists('extension', $path_info);
+	public function fiad_auto_convert_file_name($filename, $filename_raw){
+		$sanitized_filename = str_replace('%20', '-', $filename_raw); // Replace %20 with -
 
-		// Santize filename
-		$sanitized_filename = sanitize_file_name($filename);
-		$sanitized_filename = wp_unique_filename($dirname, $sanitized_filename);
-
-		$fiber_path = $dirname . "/" . $sanitized_filename . "." . $extension;
-
-		// Rename the file and update it's location in WP
-		rename($original_path, $fiber_path);
-		update_attached_file($post_id, $fiber_path);
-
-		// Register filter to update metadata.
-		add_filter('wp_update_attachment_metadata', function($data, $post_id) use ($fiber_path){
-			return wp_generate_attachment_metadata($post_id, $fiber_path);
-		}, 10, 2);
+		$sanitized_filename = remove_accents($sanitized_filename); // Convert accent char to regular char
+		$sanitized_filename = preg_replace('/[^A-Za-z0-9-. ]/', '-', $sanitized_filename); // Remove all non-alphanumeric except .
+		$sanitized_filename = preg_replace('/\.+/', '.', $sanitized_filename); // Replace a row of . with only 1 .
+		$sanitized_filename = preg_replace('/-+/', '-', $sanitized_filename); // Replace a row of - with only 1 -
+		$sanitized_filename = str_replace('-.', '.', $sanitized_filename); // Remove last - if at the end
+		
+		$fiber_filename = strtolower($sanitized_filename); // Lowercase
+		
+		return $fiber_filename;
 	}
 }
 
