751c517e530905bc8b12ccdf24182f2c0899db2c
phongkhuu115 <phongkhuu115@gmail.com>
2023-06-04 17:15:03 +0700

    feat(maintenance-mode): #7 get template-part to display content

--- includes/maintenance-mode.php
+++ includes/maintenance-mode.php
@@ -19,8 +19,24 @@ class Fiber_Admin_Maintenance_Mode{
 	
 	public function fiad_enable_maintenance_mode(){
 		if(!current_user_can('edit_themes') || !is_user_logged_in()){
-			wp_die('<h1>Under Maintenance</h1><br />Website under planned maintenance. Please check back later.');
+			$selected_post_types = fiad_get_maintenance_mode_option('maintenance_mode_page');
+			$id = $selected_post_types[0];
+			global $post;
+			$post = get_post($id);
+			setup_postdata($id);
+			$content = $this->fiad_maintenance_content();
+			
+			wp_die($content);
 		}
+	}
+	
+	public function fiad_maintenance_content(){
+		$html = '';
+		ob_start();
+		get_template_part('partials/maintenance-mode');
+		$html .= ob_get_clean();
+		
+		return $html;
 	}
 }
 
