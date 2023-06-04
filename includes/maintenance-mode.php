64349c19f05ae1f2dea925e75f8d9accb676ba29
phongkhuu115 <phongkhuu115@gmail.com>
2023-06-04 15:44:58 +0700

    feat(maintenance-mode): #7 simple way

--- includes/maintenance-mode.php
+++ includes/maintenance-mode.php
@@ -12,7 +12,14 @@ class Fiber_Admin_Maintenance_Mode{
 	public function __construct(){
 		// Enable Maintenance Mode
 		if(fiad_get_maintenance_mode_option('enable_maintenance_mode')){
-			add_filter('enable_maintenance_mode', '__return_true');
+//			add_filter('enable_maintenance_mode', [$this, 'fiad_enable_maintenance_mode'], 10, 2);
+			add_action('get_header', [$this, 'fiad_enable_maintenance_mode']);
+		}
+	}
+	
+	public function fiad_enable_maintenance_mode(){
+		if(!current_user_can('edit_themes') || !is_user_logged_in()){
+			wp_die('<h1>Under Maintenance</h1><br />Website under planned maintenance. Please check back later.');
 		}
 	}
 }
