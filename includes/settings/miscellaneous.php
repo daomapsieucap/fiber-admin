0568290eb5d529d65240ab8f69d46310b2c27e48
phongkhuu115 <phongkhuu115@gmail.com>
2023-06-05 15:35:04 +0700

    feat(metadata): #16 add option field to admin site

--- includes/settings/miscellaneous.php
+++ includes/settings/miscellaneous.php
@@ -41,14 +41,6 @@ class Fiber_Admin_Miscellaneous{
 		);
 		
 		add_settings_field(
-			'auto_img_meta', // id
-			'Auto Set Image Meta', // title
-			[$this, 'fiad_auto_image_meta'], // callback
-			'fiber-admin-miscellaneous', // page
-			'fiad_image_section' // section
-		);
-		
-		add_settings_field(
 			'disable_image_protection', // id
 			'Disable Image Protection ', // title
 			[$this, 'fiad_disable_image_protection'], // callback
@@ -102,6 +94,21 @@ class Fiber_Admin_Miscellaneous{
 			'fiber-admin-miscellaneous', // page
 			'fiad_content_section' // section
 		);
+		
+		add_settings_section(
+			'fiad_attachment_section',
+			'<span class="dashicons dashicons-format-image"></span> Attachment',
+			[$this, 'fiad_section_info'],
+			'fiber-admin-miscellaneous'
+		);
+		
+		add_settings_field(
+			'auto_attachment_meta', // id
+			'Auto Add Attachment Meta Data', // title
+			[$this, 'fiad_auto_attachment_meta'], // callback
+			'fiber-admin-miscellaneous', // page
+			'fiad_attachment_section' // section
+		);
 	}
 	
 	public function fiad_section_info(){
@@ -113,18 +120,6 @@ class Fiber_Admin_Miscellaneous{
             <label for="enable_auto_update" class="fiber-admin-toggle">
                 <input type="checkbox" name="fiad_miscellaneous[enable_auto_update]" id="enable_auto_update"
                        value="yes" <?php checked(esc_attr(fiad_get_miscellaneous_option('enable_auto_update')), 'yes'); ?> />
-                <span class="slider round"></span>
-            </label>
-        </fieldset>
-		<?php
-	}
-	
-	public function fiad_auto_image_meta(){
-		?>
-        <fieldset>
-            <label for="auto_img_meta" class="fiber-admin-toggle">
-                <input type="checkbox" name="fiad_miscellaneous[auto_img_meta]" id="auto_img_meta"
-                       value="yes" <?php checked(esc_attr(fiad_get_miscellaneous_option('auto_img_meta')), 'yes'); ?> />
                 <span class="slider round"></span>
             </label>
         </fieldset>
@@ -206,6 +201,19 @@ class Fiber_Admin_Miscellaneous{
                 <input type="checkbox" name="fiad_miscellaneous[enable_comments]"
                        id="enable_comments"
                        value="yes" <?php checked(esc_attr(fiad_get_miscellaneous_option('enable_comments')), 'yes'); ?> />
+                <span class="slider round"></span>
+            </label>
+        </fieldset>
+		<?php
+	}
+	
+	public function fiad_auto_attachment_meta(){
+		?>
+        <fieldset>
+            <label for="auto_attachment_meta" class="fiber-admin-toggle">
+                <input type="checkbox" name="fiad_miscellaneous[auto_attachment_meta]"
+                       id="auto_attachment_meta"
+                       value="yes" <?php checked(esc_attr(fiad_get_miscellaneous_option('auto_attachment_meta')), 'yes'); ?> />
                 <span class="slider round"></span>
             </label>
         </fieldset>
