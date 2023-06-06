76fd3ceb86132ae0a3c1250b79f6b1cb7f05d623
phongkhuu115 <phongkhuu115@gmail.com>
2023-06-06 09:38:13 +0700

    refactor(metadata): #16 rename class

--- includes/attachment.php
+++ includes/attachment.php
@@ -8,7 +8,7 @@ if(!defined('ABSPATH')){
 /**
  * Cleanup attachment name
  */
-class Fiber_Admin_Filename{
+class Fiber_Admin_Attachment{
 	public function __construct(){
 		// Cleanup attachment name
 		add_filter('sanitize_file_name_chars', [$this, 'fiad_special_chars']);
@@ -98,4 +98,4 @@ class Fiber_Admin_Filename{
 	}
 }
 
-new Fiber_Admin_Filename();
+new Fiber_Admin_Attachment();
