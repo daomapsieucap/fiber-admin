85d2aa38d5bfcbdaafac2a628142a15f87fbb2cf
phongkhuu115 <phongkhuu115@gmail.com>
2023-06-07 16:55:40 +0700

    feat(maintenance): #7 use page template for maintenance mode

--- includes/maintenance-mode.php
+++ includes/maintenance-mode.php
@@ -12,31 +12,43 @@ class Fiber_Admin_Maintenance_Mode{
 	public function __construct(){
 		// Enable Maintenance Mode
 		if(fiad_get_maintenance_mode_option('enable_maintenance_mode')){
-//			add_filter('enable_maintenance_mode', [$this, 'fiad_enable_maintenance_mode'], 10, 2);
-			add_action('get_header', [$this, 'fiad_enable_maintenance_mode']);
+			add_action('template_redirect', [$this, 'fiad_enable_maintenance_mode']);
+			add_filter('template_include', [$this, 'fiad_maintenance_content']);
 		}
 	}
 	
 	public function fiad_enable_maintenance_mode(){
 		if(!current_user_can('edit_themes') || !is_user_logged_in()){
-			$selected_post_types = fiad_get_maintenance_mode_option('maintenance_mode_page');
-			$id = $selected_post_types[0];
-			global $post;
-			$post = get_post($id);
-			setup_postdata($id);
-			$content = $this->fiad_maintenance_content();
-			
-			wp_die($content);
+			if(is_front_page()){
+				$selected_post_types = fiad_get_maintenance_mode_option('maintenance_mode_page');
+				$maintenance_page_id = (int) $selected_post_types[0];
+				
+				wp_redirect(get_permalink($maintenance_page_id));
+				exit();
+			}
 		}
 	}
 	
-	public function fiad_maintenance_content(){
-		$html = '';
-		ob_start();
-		get_template_part('partials/maintenance-mode');
-		$html .= ob_get_clean();
+	//No Header & Footer Page
+	public function fiad_maintenance_content($template){
+		$this->fiad_create_template_if_not_exists();
+		$new_template = dirname(__FILE__) . '/templates/maintenance.php';
+		if($new_template){
+			return $new_template;
+		}
 		
-		return $html;
+		return $template;
+	}
+	
+	public function fiad_create_template_if_not_exists(){
+		$templates_file_path = dirname(__FILE__) . '/templates/maintenance.php';
+		$html                = '';
+		if(!file_exists($templates_file_path)){
+			fopen($templates_file_path, 'w');
+			$html .= "<?php" .  "/* Template Name: Maintenance */" . "?>";
+			$html .= "<div class='fiad-maintenance-content'>" . get_the_content() . "</div>";
+			file_put_contents($templates_file_path, $html);
+		}
 	}
 }
 
