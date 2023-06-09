4a7b6cb45059daa02d8b48a84b2a3ec6a10165af
phongkhuu115 <phongkhuu115@gmail.com>
2023-06-09 14:14:30 +0700

    fix(maintenance): #7 fix redirect from only front page

--- includes/maintenance-mode.php
+++ includes/maintenance-mode.php
@@ -21,10 +21,9 @@ class Fiber_Admin_Maintenance_Mode{
 	
 	public function fiad_enable_maintenance_mode(){
 		if(!current_user_can('edit_themes') || !is_user_logged_in()){
-			if(is_front_page()){
-				$selected_post_types = fiad_get_maintenance_mode_option('maintenance_mode_page');
-				$maintenance_page_id = (int) $selected_post_types[0];
-				
+			$selected_post_types = fiad_get_maintenance_mode_option('maintenance_mode_page');
+			$maintenance_page_id = (int) $selected_post_types[0];
+			if(!is_page($maintenance_page_id)){
 				wp_redirect(get_permalink($maintenance_page_id));
 				exit();
 			}
