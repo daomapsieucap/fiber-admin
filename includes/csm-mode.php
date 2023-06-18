b0b9da3efbedf4d3b672c06813efdd4f8b74d620
phongkhuu115 <phongkhuu115@gmail.com>
2023-06-18 23:18:22 +0700

    fix(csm-mode): #7 remove redirection

--- includes/csm-mode.php
+++ includes/csm-mode.php
@@ -16,30 +16,9 @@ class Fiber_Admin_CSM_Mode{
 		$this->fiad_create_template_if_not_exists();
 		add_action('template_redirect', [$this, 'fiad_preview_csm_page']);
 		if(fiad_get_csm_mode_option('enable')){
-			add_action('template_redirect', [$this, 'fiad_enable_csm_mode']);
 			add_filter('template_include', [$this, 'fiad_csm_content']);
 			add_action('wp_head', [$this, 'fiad_csm_extra_css']);
 			add_action('wp_footer', [$this, 'fiad_csm_extra_js']);
-		}
-	}
-	
-	public function fiad_enable_csm_mode(){
-		if(!current_user_can('edit_themes') || !is_user_logged_in()){
-			if(fiad_get_csm_mode_option('enable')){
-				$title = $this->mode === 'maintenance' ? 'Maintenance' : 'Coming Soon';
-				if(!($page_id = post_exists($title))){
-					$page_id = wp_insert_post([
-						'post_title'  => $title,
-						'post_name'   => $this->mode,
-						'post_status' => 'publish',
-						'post_type'   => 'page',
-					]);
-				}
-				if(!is_page($page_id)){
-					wp_redirect(get_permalink($page_id));
-					exit();
-				}
-			}
 		}
 	}
 	
