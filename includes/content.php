de078b677cd0f5cf6d68e3c45475f8606619fff8
zrvincent <pqkhanhgl@gmail.com>
2023-06-23 11:33:08 +0700

    refactor(content-protection): #17 update script content protection and enqueue content protection script for admin

--- includes/content.php
+++ includes/content.php
@@ -9,23 +9,33 @@ if(!defined('ABSPATH')){
  */
 class Fiber_Admin_Content{
 	public function __construct(){
+		// enqueue scripts
+		add_action('admin_enqueue_scripts', [$this, 'fiad_content_scripts']);
+		
 		// Convert email text to link
 		if(!fiad_get_miscellaneous_option('disable_email_converter')){
-			add_filter('the_content', array($this, 'fiad_auto_convert_email_address'));
+			add_filter('the_content', [$this, 'fiad_auto_convert_email_address']);
 			
 			// Divi theme
-			add_filter('et_builder_render_layout', array($this, 'fiad_auto_convert_email_address'));
+			add_filter('et_builder_render_layout', [$this, 'fiad_auto_convert_email_address']);
 		}
 		
 		// Content protection
 		if(!fiad_is_admin_user_role()){
-			//Disable prevent copy content
-			if (!fiad_get_miscellaneous_option('disable_content_protection')){
-				add_action('wp_footer', array($this, 'fiad_content_protection_scripts'));
-			}
-			// Disable right click and drag on image
-			if (!fiad_get_miscellaneous_option('disable_image_protection')){
-				add_action('wp_footer', array($this, 'fiad_image_protection_scripts'));
+			if(!fiad_get_miscellaneous_option('disable_protection')){
+				//Disable prevent copy content
+				add_action('wp_footer', [$this, 'fiad_content_protection_scripts']);
+				// Disable right click and drag on image
+				add_action('wp_footer', [$this, 'fiad_image_protection_scripts']);
+			}else{
+				//Disable prevent copy content
+				if(!fiad_get_miscellaneous_option('disable_content_protection')){
+					add_action('wp_footer', [$this, 'fiad_content_protection_scripts']);
+				}
+				// Disable right click and drag on image
+				if(!fiad_get_miscellaneous_option('disable_image_protection')){
+					add_action('wp_footer', [$this, 'fiad_image_protection_scripts']);
+				}
 			}
 		}
 		
@@ -36,19 +46,31 @@ class Fiber_Admin_Content{
 		}
 		$revision_number = intval($revision_number);
 		if($revision_number > 0){
-			add_filter('wp_revisions_to_keep', array($this, 'fiad_limit_wp_revisions'), 10, 2);
+			add_filter('wp_revisions_to_keep', [$this, 'fiad_limit_wp_revisions'], 10, 2);
 		}
 		
 		// Disable comments
 		if(!fiad_get_miscellaneous_option('enable_comments')){
 			// Remove from admin menu
-			add_action('admin_menu', array($this, 'fiad_remove_comment_admin_menus'));
+			add_action('admin_menu', [$this, 'fiad_remove_comment_admin_menus']);
 			
 			// Remove from post types
-			add_action('init', array($this, 'fiad_remove_comment_support'), 100);
+			add_action('init', [$this, 'fiad_remove_comment_support'], 100);
 			
 			// Remove from admin bar
-			add_action('wp_before_admin_bar_render', array($this, 'fiad_remove_comment_admin_bar'));
+			add_action('wp_before_admin_bar_render', [$this, 'fiad_remove_comment_admin_bar']);
+		}
+	}
+	
+	public function fiad_content_scripts(){
+		if(is_admin()){
+			// scripts
+			$suffix = '';
+			if(!FIBERADMIN_DEV_MODE){
+				$suffix = '.min';
+			}
+			wp_enqueue_script('fiber-admin-content', FIBERADMIN_ASSETS_URL . 'js/fiber-content' . $suffix . '.js', false, FIBERADMIN_VERSION, true);
+			
 		}
 	}
 	
@@ -73,8 +95,8 @@ class Fiber_Admin_Content{
 		}
 		
 		// Detect and create email link
-		$search  = array('/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,})/');
-		$replace = array('<a href="mailto:$1" title="$1">$1</a>');
+		$search  = ['/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,})/'];
+		$replace = ['<a href="mailto:$1" title="$1">$1</a>'];
 		
 		return preg_replace($search, $replace, $content);
 	}
@@ -82,11 +104,10 @@ class Fiber_Admin_Content{
 	public function fiad_content_protection_scripts(){
 		echo '
 			<script type="text/javascript">
+				const disable_keys = ["s","a","c","x","C","J","U","I"];
 				document.addEventListener("keydown", function(e){
-			        if((navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)){
-			            if(e.key === "s" || e.key === "a" || e.key === "c" || e.key === "x" || (e.shiftKey && e.key === "I")){
-			                e.preventDefault();
-			            }
+			        if(((navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey) && disable_keys.includes(e.key)) || e.key === "F12"){
+           				e.preventDefault();
 			        }
 			    }, false);
                 
