016d2bd4ea5ed29bcd4024b7d3ea3ea9bd6b17c6
phongkhuu115 <phongkhuu115@gmail.com>
2023-07-12 17:16:18 +0700

    refactor(csm-mode): remove create page code

--- includes/csm-mode.php
+++ includes/csm-mode.php
@@ -9,13 +9,12 @@ if(!defined('ABSPATH')){
  */
 class Fiber_Admin_CSM_Mode{
 	private $mode = '';
+	private $page_id = '';
 	
 	public function __construct(){
 		// Enable Coming Soon/Maintenance Mode
-		$this->mode = fiad_get_csm_mode_option('mode');
-		$this->fiad_create_template_if_not_exists();
-		add_action('template_redirect', [$this, 'fiad_preview_csm_page']);
 		if(fiad_get_csm_mode_option('enable')){
+			$this->mode = fiad_get_csm_mode_option('mode');
 			add_filter('template_include', [$this, 'fiad_csm_content']);
 			add_action('wp_head', [$this, 'fiad_csm_extra_css']);
 			add_action('wp_footer', [$this, 'fiad_csm_extra_js']);
@@ -40,55 +39,6 @@ class Fiber_Admin_CSM_Mode{
 		}
 		
 		return $template;
-	}
-	
-	public function fiad_create_template_if_not_exists(){
-		$file_name           = $this->mode . '.php';
-		$templates_file_path = FIBERADMIN_TEMPLATES_URL . $file_name;
-		$selected_page       = fiad_get_csm_mode_option('page');
-		$html                = '';
-		
-		if(!file_exists(FIBERADMIN_TEMPLATES_URL)){
-			mkdir(FIBERADMIN_TEMPLATES_URL);
-		}
-		fopen($templates_file_path, 'w');
-		
-		$title = get_bloginfo('name');
-		global $post;
-		$post = get_post($selected_page);
-		setup_postdata($post);
-		$content = preg_replace('#\[[^\]]+\]#', '', $post->post_content);
-		
-		if($this->mode == 'maintenance'){
-			$php = '<?php';
-			$php .= PHP_EOL;
-			$php .= 'header(\'HTTP/1.1 503 Service Temporarily Unavailable\', true, 503 );';
-			$php .= PHP_EOL;
-			$php .= 'header(\'Status: 503 Service Temporarily Unavailable\');';
-			$php .= PHP_EOL;
-			$php .= 'header(\'Retry-After: 3600\');';
-			$php .= PHP_EOL;
-			$php .= '?>';
-			
-			$html .= $php;
-		}
-		$html .= '<!DOCTYPE HTML>';
-		$html .= '<html ' . get_language_attributes() . '>';
-		$html .= '<head>';
-		$html .= '<title>' . $title . '</title>';
-		$html .= '<link rel="icon" type="image/png" href="' . get_site_icon_url() . '"/>';
-		$html .= '<?php wp_head(); ?>';
-		$html .= '</head>';
-		$html .= '<body>';
-		$html .= '<div class="fiad-' . $this->mode . '-content">';
-		$html .= ev_vc_content($content);
-		$html .= '</div>';
-		$html .= '<?php wp_footer(); ?>';
-		$html .= '</body>';
-		$html .= '</html>';
-		file_put_contents($templates_file_path, $html);
-		
-		wp_reset_postdata();
 	}
 	
 	public function fiad_csm_extra_css(){
