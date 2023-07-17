d2a594348c1c4b9c91c96aae95d9fed51666026d
phongkhuu115 <phongkhuu115@gmail.com>
2023-07-17 14:40:33 +0700

    fix(csm-mode): #7 dequeue only for csm mode and not admin

--- includes/csm-mode.php
+++ includes/csm-mode.php
@@ -17,6 +17,7 @@ class Fiber_Admin_CSM_Mode{
 		// Apply for both enable and preview mode
 		$this->fiad_create_default_csm_page();
 		$this->fiad_add_default_css();
+		add_action('wp_enqueue_scripts', [$this, 'fiad_dequeue_all_for_csm'], 15);
 		add_filter('fiad_csm_extra_css', [$this, 'fiad_csm_extra_css']);
 		add_filter('fiad_csm_extra_js', [$this, 'fiad_csm_extra_js']);
 		add_filter('template_include', [$this, 'fiad_preview_csm_page']);
@@ -63,7 +64,6 @@ class Fiber_Admin_CSM_Mode{
 			$default_extra_css = "body { text-align: center; padding: 150px; }\n";
 			$default_extra_css .= "h1 { font-size: 50px; }\n";
 			$default_extra_css .= "body { font: 20px Helvetica, sans-serif; color: #333; }\n";
-			$default_extra_css .= "article { display: block; text-align: left; width: 650px; margin: 0 auto; }\n";
 			
 			$csm_mode_option['csm_extra_css'] = $default_extra_css;
 			$csm_mode_option['css_added']     = true;
@@ -71,10 +71,28 @@ class Fiber_Admin_CSM_Mode{
 		}
 	}
 	
+	public function fiad_dequeue_all_for_csm(){
+		global $wp_scripts, $wp_styles;
+		$preview_mode = sanitize_title(fiad_array_key_exists('preview', $_GET));
+		$csm_enable   = fiad_get_csm_mode_option('enable');
+		
+		if(!fiad_is_admin_user_role()){
+			if($preview_mode || $csm_enable){
+				foreach($wp_scripts->queue as $handle){
+					wp_dequeue_script($handle);
+				}
+				
+				foreach($wp_styles->queue as $handle){
+					wp_dequeue_style($handle);
+				}
+			}
+		}
+	}
+	
 	public function fiad_create_default_csm_page(){
-		$pages_added     = fiad_get_csm_mode_option('page_added');
-		$csm_mode_option = get_option('fiad_csm_mode');
-		$page_titles     = [
+		$pages_added                   = fiad_get_csm_mode_option('page_added');
+		$csm_mode_option               = get_option('fiad_csm_mode');
+		$page_titles                   = [
 			'coming-soon' => 'Coming Soon',
 			'maintenance' => 'Maintenance',
 		];
